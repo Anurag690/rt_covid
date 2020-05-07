52c39b088eaa1deb72f9c895da444877d8ffb105
anurag.gupta690@gmail.com <anurag.gupta690@gmail.com>
2020-05-07 20:42:41 +0530

    note

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -22,6 +22,7 @@ export default class App extends React.PureComponent{
                 <Header/>
                 <CountryChart/>
                 <StateCharts/>
+                <div style={{fontWeight: "bold", padding: '0 7% 2%', color: 'rgb(169,169,169)'}}>* States with less than 100 cases have been removed because of insufficient data.</div>
                 <Footer/>
             </div>
         );
